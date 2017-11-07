30d114d1d60fee8a779ce0bd9bb0e71c13fedb85
Nick Ruiz <nickruiz210@gmail.com>
2017-11-06 20:52:10 -0600

    Formatting Exception class to test my first commit

--- src/planet/detail/InvalidPlanetException.java
+++ src/planet/detail/InvalidPlanetException.java
@@ -1,8 +1,11 @@
 package planet.detail;
+
 public class InvalidPlanetException extends RuntimeException {
+	
 	public InvalidPlanetException(RuntimeException e){
 		super(e);
 	}
+	
 	public InvalidPlanetException(String message){
 		super(message);
 	}
